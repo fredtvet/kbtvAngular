0c3ce5056ea8ef3cae72ab1ad20a487a112ff400
Fredrik Tvete <fretve@hotmail.com>
2020-05-10 16:00:06 +0200

    update to main nav interface after refactoring

--- src/app/shared/interfaces/main-nav-config.interface.ts
+++ src/app/shared/interfaces/main-nav-config.interface.ts
@@ -1,23 +1,17 @@
 import { AppButton } from './app-button.interface';
 
 export interface MainNavConfig{
-
-    altNav: boolean;
     title: string;
-    icon: string;
-    subTitle:string;
-    subIcon: string;
+    multiLineTitle?: string[];
+
+    subTitle?:string;
+    subIcon?: string;
 
     buttons?: AppButton[];
-    
-    searchFn?: Function;
+    bottomSheetButtons?: AppButton[];
+
     backFn?: Function;
     backFnParams?: any[];
-    
-    bottomSheetButtons?: AppButton[];
-    
-    menuBtnEnabled: boolean;
-    elevationEnabled: boolean;
-
-    context?: any;
+       
+    elevationEnabled?: boolean;
 }
