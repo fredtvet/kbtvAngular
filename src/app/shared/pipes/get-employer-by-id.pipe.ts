f1c0fae863a27dd6df53e9b0a3107c95f9f75830
Fredrik Tvete <fretve@hotmail.com>
2020-05-19 19:49:17 +0200

    employerbyid pipe

---
+++ src/app/shared/pipes/get-employer-by-id.pipe.ts
@@ -0,0 +1,25 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { EmployerService } from 'src/app/core/services';
+import { Employer } from '../models';
+import { SubscriptionComponent } from '../components/abstracts/subscription.component';
+import { takeUntil } from 'rxjs/operators';
+
+@Pipe({
+  name: 'getEmployerById',
+  pure: false
+  
+})
+export class GetEmployerByIdPipe extends SubscriptionComponent implements PipeTransform {
+
+  result: Employer = null;
+
+  constructor(private employerService: EmployerService){super()}
+
+  transform(value: number): Employer {   
+    if(!value || value == null) return undefined;  
+    console.log(this.result);
+    this.employerService.get$(value).pipe(takeUntil(this.unsubscribe)).subscribe(x => this.result = x);
+    return this.result;
+  }
+
+}
