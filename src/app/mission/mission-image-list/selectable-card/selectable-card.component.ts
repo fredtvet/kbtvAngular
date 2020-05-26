1f8314af87eaa1a74f1102de32c8d336b458eeb7
Fredrik Tvete <fretve@hotmail.com>
2020-05-27 00:36:13 +0200

    selectable card component

---
+++ src/app/mission/mission-image-list/selectable-card/selectable-card.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-selectable-card',
+  templateUrl: './selectable-card.component.html',
+  styleUrls: ['./selectable-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class SelectableCardComponent {
+
+  @Input() selectedText: string;
+  @Input() selectedIcon: string;
+  @Input() isSelected: boolean = false;
+  
+  @Output() selected = new EventEmitter<boolean>()
+
+  constructor() { }
+
+  toggleSelection = () => {
+    this.isSelected = !this.isSelected;
+    this.selected.emit(this.isSelected);
+  };
+  
+}
