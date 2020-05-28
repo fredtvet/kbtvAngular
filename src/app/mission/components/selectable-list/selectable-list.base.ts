90f5c5d37833367ccd2e963155899c788643ae1c
Fredrik Tvete <fretve@hotmail.com>
2020-05-28 14:40:17 +0200

    generic selectable list

---
+++ src/app/mission/components/selectable-list/selectable-list.base.ts
@@ -0,0 +1,35 @@
+import {
+    Input,
+    EventEmitter,
+    Output,
+  } from "@angular/core";
+  import { skip, takeUntil } from "rxjs/operators";
+  import { SelectableEntity, AppFile, BaseEntity } from 'src/app/shared/interfaces';
+import { SelectableListPresenter } from './selectable-list.presenter';
+import { SubscriptionComponent } from 'src/app/shared/components/abstracts/subscription.component';
+  
+  export abstract class SelectableListBase<T extends BaseEntity> extends SubscriptionComponent {
+    @Input('entities')
+    set entities(value: T[]) {this.selectableListPresenter.addEntities(value)}
+  
+    @Output() selectionChanged = new EventEmitter<number[]>();
+  
+    selectableEntities$ = this.selectableListPresenter.selectableEntities$;
+  
+    constructor(protected selectableListPresenter: SelectableListPresenter<T>) {super();}
+    
+    ngOnInit(): void {
+        this.selectableListPresenter.selectedIds$.pipe(
+            skip(1),
+            takeUntil(this.unsubscribe)
+        ).subscribe(x => this.selectionChanged.emit(x))
+    }
+  
+    toggleSelect(selectable: SelectableEntity<T>) {
+      this.selectableListPresenter.toggleEntity(selectable.entity.id)
+    }
+  
+    clearSelections = () => this.selectableListPresenter.addSelections([]);
+  
+  }
+  
---
+++ src/app/mission/components/selectable-list/selectable-list.presenter.ts
@@ -0,0 +1,50 @@
+import { SubscriptionComponent } from 'src/app/shared/components/abstracts/subscription.component';
+import { BaseEntity, SelectableEntity } from 'src/app/shared/interfaces';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export class SelectableListPresenter<T extends BaseEntity> extends SubscriptionComponent{
+
+    private selectedIdsSubject = new BehaviorSubject<number[]>([]);
+    selectedIds$ = this.selectedIdsSubject.asObservable();
+
+    private entitiesSubject =  new BehaviorSubject<T[]>([]);
+    private entities$ = this.entitiesSubject.asObservable();
+
+    selectableEntities$: Observable<SelectableEntity<T>[]> = combineLatest(this.entities$, this.selectedIds$).pipe(
+        map(([entities, selectedIds]) => this.getSelectableEntities(entities, selectedIds))
+    )
+
+    constructor(){super()}
+    
+    get entities(){
+        return [...this.entitiesSubject.value]
+    }
+
+    addEntities(entities: T[]){
+        this.entitiesSubject.next(entities);
+    }
+
+    addSelections(ids: number[]){
+        this.selectedIdsSubject.next(ids);
+    }
+
+    toggleEntity(id: number){
+        let ids = [...this.selectedIdsSubject.value];
+        let index = ids.findIndex(x => x == id);
+        if(index == -1) ids.push(id); //if not selected
+        else ids.splice(index, 1);
+        this.selectedIdsSubject.next(ids);
+    }
+
+    isEntitySelected = (id:number) => this.selectedIdsSubject.value.includes(id);
+    
+    private getSelectableEntities(entities:T[], selectedIds:number[]): SelectableEntity<T>[]{
+        let result = [];
+        for(let i = 0; i < entities.length; i++){
+            let isSelected = selectedIds.includes(entities[i].id);
+            result.push({entity: entities[i], selected:isSelected})
+        }
+        return result;
+    }
+}
