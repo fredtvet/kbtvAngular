2370578d76bbb67c4ec84b30ea3336661931fcc2
Fredrik Tvete <fretve@hotmail.com>
2020-06-11 11:34:01 +0200

    new empty module

---
+++ src/app/mission-modules/mission-document-list/mission-document-list.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MissionDocumentListRoutingModule } from './mission-document-list-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MissionDocumentListRoutingModule
+  ]
+})
+export class MissionDocumentListModule { }
