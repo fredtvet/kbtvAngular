18d4de602fe83466eb45af30105e1f5c3c0d9ed5
Fredrik Tvete <fretve@hotmail.com>
2020-06-22 13:54:27 +0200

    change filter function

--- src/app/timesheet-modules/timesheet-user-modules/timesheet/timesheet-list/timesheet-list.component.ts
+++ src/app/timesheet-modules/timesheet-user-modules/timesheet/timesheet-list/timesheet-list.component.ts
@@ -5,7 +5,7 @@ import { TimesheetStatus, DateRangePresets } from "src/app/shared-app/enums";
 import { ActivatedRoute, Router } from "@angular/router";
 import { Mission, Timesheet } from "src/app/core/models";
 import { BehaviorSubject, Observable } from "rxjs";
-import { switchMap, filter } from "rxjs/operators";
+import { switchMap, filter, tap, distinctUntilChanged } from "rxjs/operators";
 import { TopDefaultNavConfig } from 'src/app/shared-app/interfaces';
 import { TimesheetListFilter } from 'src/app/shared/timesheet-list-filter.model';
 import { TimesheetFilterSheetWrapperComponent } from 'src/app/timesheet-modules/shared-timesheet/components';
@@ -36,19 +36,13 @@ export class TimesheetListComponent implements OnInit {
     //Initiate filter and observable
     this.filterSubject = new BehaviorSubject(this.getInitialFilter());
 
-    this.timesheets$ = this.filterSubject.asObservable().pipe(  
-      switchMap(filter => this.userTimesheetService.getByWithMission$(t => filter.checkTimesheet(t)))
+    this.timesheets$ = this.filterSubject.asObservable().pipe(tap(console.log), distinctUntilChanged(),
+      switchMap(filter => this.userTimesheetService.getByWithMission$(filter.checkTimesheet))
     );
   }
 
   openTimesheetForm = (missionPreset?: Mission, idPreset?: number) => 
     this.router.navigate(['skjema'], {relativeTo: this.route, queryParams: {idPreset, missionPreset: JSON.stringify(missionPreset)}});
-
-  changeStatus(status: TimesheetStatus) {
-    let filter = this.getFilterCopy();
-    filter.status = status;
-    this.filterSubject.next(filter);
-  }
 
   deleteTimesheet = (id: number) => this.userTimesheetService.delete$(id).subscribe();
 
@@ -93,9 +87,7 @@ export class TimesheetListComponent implements OnInit {
     } else {
       //Default year preset if no input
       filter.dateRangePreset = DateRangePresets.CurrentYear;
-      filter.dateRange = this.dateTimeService.getRangeByDateRangePreset(
-        DateRangePresets.CurrentYear
-      );
+      filter.dateRange = this.dateTimeService.getRangeByDateRangePreset(filter.dateRangePreset);
     }
     return filter;
   }
