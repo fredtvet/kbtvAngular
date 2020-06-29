34fdb879b45a958fd09238c15a0419e389734afe
Fredrik Tvete <fretve@hotmail.com>
2020-06-29 16:09:37 +0200

    replace custom csv export with ag grid export ...

--- src/app/timesheet-modules/timesheet-statistic/timesheet-statistic/timesheet-statistic.component.ts
+++ src/app/timesheet-modules/timesheet-statistic/timesheet-statistic/timesheet-statistic.component.ts
@@ -1,10 +1,11 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { MainNavService, TimesheetService, JsonToCsvExportService, TranslationService } from 'src/app/core/services';
-import { TopDefaultNavConfig, TimesheetFilter, TimesheetSummary } from 'src/app/shared-app/interfaces';
+import { Component, ChangeDetectionStrategy, ViewChild } from '@angular/core';
+import { MainNavService, TimesheetService, JsonToCsvExportService } from 'src/app/core/services';
+import { TopDefaultNavConfig, TimesheetFilter} from 'src/app/shared-app/interfaces';
 import { GroupByTypes } from 'src/app/shared-app/enums';
 import { TimesheetFilterSheetWrapperComponent } from '../../shared-timesheet/components/timesheet-filter-sheet-wrapper.component';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { filter, tap } from 'rxjs/operators';
+import { TimesheetStatisticTableComponent } from './timesheet-statistic-table/timesheet-statistic-table.component';
 
 @Component({
   selector: 'app-timesheet-statistic',
@@ -12,18 +13,18 @@ import { filter, tap } from 'rxjs/operators';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TimesheetStatisticComponent {
+  @ViewChild('statTable') statTable: TimesheetStatisticTableComponent;
+
   groupByTypes = GroupByTypes;
 
   filter$ = this.timesheetService.filter$;
   groupBy$ = this.timesheetService.groupBy$;
-  timesheetSummaries$ = this.timesheetService.timesheetSummaries$.pipe(tap(x => this.summaries = x));
-  private summaries = []; //Private store for using with layout service
+  timesheetSummaries$ = this.timesheetService.timesheetSummaries$;
 
   constructor(
     private mainNavService: MainNavService,
     private timesheetService: TimesheetService,
-    private bottomSheet: MatBottomSheet,
-    private jsonToCsvExportService: JsonToCsvExportService
+    private bottomSheet: MatBottomSheet
     ) { 
       this.configureMainNav();
     }
@@ -40,22 +41,12 @@ export class TimesheetStatisticComponent {
       .subscribe(x => this.timesheetService.addFilter(x));
   }
 
-  private exportSummariesToCsv = () => {
-    if(!this.summaries || this.summaries.length === 0) return undefined;  
-    var ignoredProperties = {timesheets: true, userName: true};
-    var objects: Object[] = [];
-    for(var d = 0; d < this.summaries.length; d++){
-      var clone = {...this.summaries[d]};
-      Object.getOwnPropertyNames(ignoredProperties).forEach(x => delete clone[x]);
-      objects.push(clone);
-    }
-    this.jsonToCsvExportService.exportJsonToCsv$(objects).subscribe(x => console.log(x))
-  }
-
+  private exportAsCsv = () => this.statTable.dataGrid.api.exportDataAsCsv();
+  
   private configureMainNav(){
     let cfg = {
       title:  "Timestatistikk",
-      bottomSheetButtons: [{icon: "import_export", text: "Eksporter timer til CSV", callback: this.exportSummariesToCsv}]
+      bottomSheetButtons: [{icon: "import_export", text: "Eksporter timer til CSV", callback: this.exportAsCsv}]
     } as TopDefaultNavConfig;
     
     this.mainNavService.addTopNavConfig({default: cfg});
