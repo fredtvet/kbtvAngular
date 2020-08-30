850547a8080345858ab0ca59b14ef587f692d9b0
Fredrik Tvete <fretve@hotmail.com>
2020-08-30 22:03:03 +0200

    add nav searchbar

---
+++ src/app/layout/main-nav-config.interface.ts
@@ -0,0 +1,42 @@
+import { AppButton } from '../shared-app/interfaces/app-button.interface';
+import { User } from 'src/app/core/models';
+
+export interface MainNavConfig{
+    isXs?: boolean;
+    currentUser?: User;
+
+    topDefaultNavConfig?: TopDefaultNavConfig;
+    topDetailNavConfig?: TopDetailNavConfig;  
+
+    fabs?: AppButton[];
+}
+
+export interface TopDefaultNavConfig extends TopNavActions{
+    title: string;
+    subTitle?:string;
+    subIcon?: string;
+
+    searchBar?: SearchBarConfig;
+
+    elevationDisabled?: boolean;
+}
+
+export interface TopDetailNavConfig extends TopNavActions{   
+    title?: string[];
+    subTitle?:string;
+    subIcon?: string;
+
+    imgSrc?: string;
+}
+
+export interface TopNavActions{
+    backFn?: Function;
+    backFnParams?: any[];
+    buttons?: AppButton[];
+    bottomSheetButtons?: AppButton[];
+}
+
+export interface SearchBarConfig{
+    callback: Function, 
+    placeholder: string
+}
--- src/app/layout/main-nav/main-top-nav/main-top-nav.component.ts
+++ src/app/layout/main-nav/main-top-nav/main-top-nav.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectionStrategy  } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy, ElementRef, ViewChild  } from '@angular/core';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { LoadingService } from 'src/app/core/services';
 import { Observable } from 'rxjs';
-import { TopDefaultNavConfig, AppButton } from 'src/app/shared-app/interfaces';
-import { BottomSheetMenuComponent } from 'src/app/shared-app/components';
+import { TopDefaultNavConfig } from '../../main-nav-config.interface';
 import { ButtonTypes } from 'src/app/shared-app/enums';
+import { AppButton } from 'src/app/shared-app/interfaces';
+import { BottomSheetMenuComponent } from 'src/app/shared-app/components';
 
 @Component({
   selector: 'app-main-top-nav',
@@ -12,12 +13,16 @@ import { ButtonTypes } from 'src/app/shared-app/enums';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainTopNavComponent {
+  @ViewChild('searchInput') searchInput: ElementRef;
+
   ButtonTypes = ButtonTypes;
   @Input() config: TopDefaultNavConfig;
   @Input() currentUserRole: string;
   @Output() menuBtnClicked = new EventEmitter();
   
   loading$: Observable<boolean> = this.loadingService.queryLoading$;
+
+  searchBarHidden: boolean = true;
 
   constructor(
     private _bottomSheet: MatBottomSheet,
@@ -26,4 +31,16 @@ export class MainTopNavComponent {
   onMenuButtonClick = () => this.menuBtnClicked.emit();
   
   openBottomSheet = (buttons: AppButton[]) => this._bottomSheet.open(BottomSheetMenuComponent, { data: buttons });
+
+  toggleSearchBar = () => {
+    if(!this.searchBarHidden) this.resetSearch(); //Reset criteria when hiding
+    this.searchBarHidden = !this.searchBarHidden
+  };
+
+  handleSearchFn = (criteria: string) => this.config.searchBar.callback(criteria);
+
+  private resetSearch(){
+    this.handleSearchFn(null); 
+    this.searchInput.nativeElement.value = "";
+  }
 }
