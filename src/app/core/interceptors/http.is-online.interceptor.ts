b76d7372dd0a207a61a7e7d369fd7855985b937a
Fredrik Tvete <fretve@hotmail.com>
2020-09-01 22:54:11 +0200

    add delay on is online interceptor

--- src/app/core/interceptors/http.is-online.interceptor.ts
+++ src/app/core/interceptors/http.is-online.interceptor.ts
@@ -1,13 +1,12 @@
-import { Injectable } from "@angular/core";
 import {
-  HttpInterceptor,
-  HttpRequest,
-  HttpHandler,
+  HttpHandler, HttpInterceptor,
+  HttpRequest
 } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable, race, throwError, timer } from "rxjs";
+import { first, map, switchMap, tap } from 'rxjs/operators';
 import { DeviceInfoService } from "../services/device-info.service";
-import { throwError, Observable } from "rxjs";
-import { tap, switchMap, take } from 'rxjs/operators';
-import { NotificationType, NotificationService } from '../services/notification';
+import { NotificationService, NotificationType } from '../services/notification';
 
 @Injectable()
 export class HttpIsOnlineInterceptor implements HttpInterceptor {
@@ -18,14 +17,17 @@ export class HttpIsOnlineInterceptor implements HttpInterceptor {
   ) {console.log("HttpIsOnlineInterceptor");}
 
   intercept(request: HttpRequest<any>, next: HttpHandler) {
-      return this.deviceInfoService.isOnline$.pipe(take(1), switchMap(isOnline => {
+    return race(
+      timer(2000).pipe(map(x => false)), //Emits false after x time IF no indication of online
+      this.deviceInfoService.isOnline$.pipe(first(x => x ===true))
+    ).pipe(switchMap(isOnline => {
         if(isOnline) return next.handle(request);
         else return this.throwNotOnlineError();
-      }))
+    }));
   }
 
   private throwNotOnlineError(): Observable<never> {
-    return throwError("Denne funksjonen krever internett tilkobling.").pipe(
+    return throwError("Får ikke koblet til internett. Eventuelle endringer har blitt reversert.").pipe(
       tap((next) => {}, (error) =>
           this.notificationService.notify({
             title: error,
