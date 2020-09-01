3b025a9f45ebd08a281eca732f740eb1fc50dfb7
Fredrik Tvete <fretve@hotmail.com>
2020-09-01 14:11:09 +0200

    fix mission filter check

--- src/app/shared/mission-filter.model.ts
+++ src/app/shared/mission-filter.model.ts
@@ -1,21 +1,54 @@
 import { Mission } from '../core/models';
+import { MissionFilterCriteria } from './interfaces';
 
 export class MissionFilter {
-    constructor(public criteria: string){ }
 
-    check(m: Mission): boolean {
-        if(!this.criteria) return this.filterInitialMission(m);
-        let exp = m.address.toLowerCase().includes(this.criteria.toLowerCase());
-        const id = +this.criteria;
-        if(!isNaN(id)) exp = exp || m.id === id //If search input is number, include id search
+    private checksCount: number = 0;
+
+    constructor(public criteria: MissionFilterCriteria, private maxChecks?: number){ console.log(criteria); 
+        console.log(undefined === false) 
+        console.log(null === false) 
+    }
+
+    check(mission: Mission): boolean{ 
+        if(!this.criteria) return true;
+        if(!mission) return false;
+        if(this.maxChecks && this.checksCount >= this.maxChecks) return false;
+        
+        let exp = true;
+
+        if(this.criteria.finished != null)
+            exp = exp && (mission.finished == null || mission.finished === this.criteria.finished);
+
+        if(this.criteria.searchString)
+            exp = exp && this.filterSearchString(mission);
+
+        if(this.criteria.employerId)
+            exp = exp && mission.employerId === this.criteria.employerId;
+
+        if(this.criteria.missionTypeId)
+            exp = exp && mission.missionTypeId === this.criteria.missionTypeId;
+
+        if(exp && this.maxChecks) this.checksCount++; //if true, append checksCount
+
         return exp;
     }
-    
-    private filterInitialMission(mission: Mission): boolean{
+
+    checkInitial(mission: Mission): boolean{
         let thirtyDaysAgo = new Date().setDate(new Date().getDate() - 30);
         let exp = new Date(mission.updatedAt).getTime() >= thirtyDaysAgo; //Grab missions updated last 30 days
         exp = exp || new Date(mission.lastVisited).getTime() >= thirtyDaysAgo;//Grab missions visited last 30 days
         return exp;
     }
+
+    private filterSearchString(m: Mission): boolean{
+        let exp = m.address.toLowerCase().includes(this.criteria.searchString.toLowerCase());
+        
+        let id = +this.criteria.searchString;
+
+        if(!isNaN(id)) exp = exp || m.id === id //Search by ID if number;
+
+        return exp
+    }  
        
 };
