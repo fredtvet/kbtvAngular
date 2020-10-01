47d08c71f796a432384c8522055c70ca843bde0f
Fredrik Tvete <fretve@hotmail.com>
2020-10-01 13:11:53 +0200

    fix summary aggregator rounding issue + small opts

--- src/app/core/services/utility/timesheet-summary.aggregator.ts
+++ src/app/core/services/utility/timesheet-summary.aggregator.ts
@@ -32,22 +32,21 @@ export class TimesheetSummaryAggregator {
       const timesheet = t[i];
       const date = new Date(timesheet.startTime);
       const index = date.toDateString() + "-" + timesheet.userName;
-      if(groups[index] === undefined) {
+      let summary = groups[index];
+      if(summary === undefined) {
         groups[index] = {
           userName: timesheet.userName,
           date,
           openHours: 0,
           confirmedHours: 0,
           timesheets: [],
-        } as TimesheetSummary;
+        };
+        summary = groups[index];  
       }
 
-      if (timesheet.status === TimesheetStatus.Open)
-        groups[index].openHours += Math.round(timesheet.totalHours * 10) / 10;
-      else
-        groups[index].confirmedHours += Math.round(timesheet.totalHours * 10) / 10;
+      this.addHoursToSummary(summary, timesheet)
 
-      groups[index].timesheets.push(timesheet);
+      summary.timesheets.push(timesheet);
     }
     const res = Object.values(groups) as TimesheetSummary[]; 
     return res
@@ -60,8 +59,8 @@ export class TimesheetSummaryAggregator {
       const timesheet = t[i];
       const wy = _getWeekAndYearFromDate(timesheet.startTime);
       const index = wy.year + "-" + wy.week + "-" + timesheet.userName;
-
-      if (groups[index] === undefined) {
+      let summary = groups[index];
+      if (summary === undefined) {
         groups[index] = {
           userName: timesheet.userName,
           year: wy.year,
@@ -70,15 +69,14 @@ export class TimesheetSummaryAggregator {
           confirmedHours: 0,
           timesheets: [],
         };
+        summary = groups[index];
       }  
 
-      if (timesheet.status === TimesheetStatus.Open)
-        groups[index].openHours += Math.round(timesheet.totalHours * 10) / 10;
-      else
-        groups[index].confirmedHours += Math.round(timesheet.totalHours * 10) / 10;
+      this.addHoursToSummary(summary, timesheet)
 
-      groups[index].timesheets.push(timesheet);
+      summary.timesheets.push(timesheet);
     }
+
     return Object.values(groups);
   }
 
@@ -92,15 +90,16 @@ export class TimesheetSummaryAggregator {
       if(date.getFullYear() !== year) continue;
       const weekNr = _getWeekOfYear(date);
       if (weekNr >= startWeek && weekNr <= endWeek) {
-        if (groups[weekNr] === undefined)
-          groups[weekNr] = { week: weekNr,timesheets: [],openHours: 0,confirmedHours: 0,} as TimesheetSummary;
+        let summary = groups[weekNr];
+        if (summary === undefined){
+          groups[weekNr] = 
+            {week: weekNr, timesheets: [],openHours: 0,confirmedHours: 0,};    
+          summary = groups[weekNr];  
+        }
 
-        groups[weekNr].timesheets.push(timesheet);
+        this.addHoursToSummary(summary, timesheet)
 
-        if (timesheet.status === TimesheetStatus.Open)
-          groups[weekNr].openHours += Math.round(timesheet.totalHours * 10) / 10;
-        else
-          groups[weekNr].confirmedHours += Math.round(timesheet.totalHours * 10) / 10;
+        summary.timesheets.push(timesheet);
       }
     };
 
@@ -116,7 +115,9 @@ export class TimesheetSummaryAggregator {
       const month = date.getMonth();
       const year = date.getFullYear();
       const index = year + "-" + month + "-" + timesheet.userName;
-      if (groups[index] === undefined) {
+      let summary = groups[index];
+
+      if (summary === undefined) {
         groups[index] = {
           userName: timesheet.userName,
           year,
@@ -124,14 +125,12 @@ export class TimesheetSummaryAggregator {
           openHours: 0, confirmedHours: 0,
           timesheets: [],
         };
+        summary = groups[index];
       }
 
-      if (timesheet.status === TimesheetStatus.Open)
-        groups[index].openHours += Math.round(timesheet.totalHours * 10) / 10;
-      else
-        groups[index].confirmedHours += Math.round(timesheet.totalHours * 10) / 10;
+      this.addHoursToSummary(summary, timesheet)
 
-      groups[index].timesheets.push(timesheet);
+      summary.timesheets.push(timesheet);
     };
 
     return Object.values(groups);
@@ -145,21 +144,28 @@ export class TimesheetSummaryAggregator {
       const year = new Date(timesheet.startTime).getFullYear();
       const index = year + "-" + timesheet.userName;
 
-      if (groups[index] === undefined) {
+      let summary = groups[index];
+
+      if (summary === undefined) {
         groups[index] = {
           userName: timesheet.userName, year, 
           openHours: 0, confirmedHours: 0, timesheets: [],
         };
+        summary = groups[index];
       }
 
-      if (timesheet.status === TimesheetStatus.Open)
-        groups[index].openHours += Math.round(timesheet.totalHours * 10) / 10;
-      else
-        groups[index].confirmedHours += Math.round(timesheet.totalHours * 10) / 10;
+      this.addHoursToSummary(summary, timesheet)
 
-      groups[index].timesheets.push(timesheet);
+      summary.timesheets.push(timesheet);
     };
     return Object.values(groups);
+  }
+
+  private addHoursToSummary(summary: TimesheetSummary, timesheet: Timesheet): void{
+    if (timesheet.status === TimesheetStatus.Open)
+      summary.openHours = Math.round((summary.openHours + timesheet.totalHours) * 10) / 10;
+    else
+      summary.confirmedHours = Math.round((summary.confirmedHours + timesheet.totalHours) * 10) / 10;
   }
 
 }
