4575e3cc9fbe6c38f978feff9cbd98b0de47a291
Fredrik Tvete <fretve@hotmail.com>
2020-10-01 18:58:59 +0200

    remove image viewer animation, use show/hide

--- src/app/mission-modules/mission-image-list/image-viewer/image-viewer.component.ts
+++ src/app/mission-modules/mission-image-list/image-viewer/image-viewer.component.ts
@@ -1,41 +1,10 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
-import { BottomSheetMenuService } from 'src/app/core/services/ui/bottom-sheet-menu.service';
 import { ModelFile } from 'src/app/core/models';
+import { BottomSheetMenuService } from 'src/app/core/services/ui/bottom-sheet-menu.service';
 import { AppButton } from 'src/app/shared-app/interfaces';
 
 @Component({
   selector: 'app-image-viewer',
-  animations: [
-    trigger('showHideToolbar', [
-      state('show', style({
-        top:'0px'
-      })),
-      state('hide', style({
-        top:'-56px'
-      })),
-      transition('show => hide', [
-        animate('.1s ease-out')
-      ]),
-      transition('hide => show', [
-        animate('.1s ease-in',)
-      ]),
-    ]),
-    trigger('showHideCounter', [
-      state('show', style({
-        bottom:'0px'
-      })),
-      state('hide', style({
-        bottom:'-56px'
-      })),
-      transition('show => hide', [
-        animate('.1s ease-out')
-      ]),
-      transition('hide => show', [
-        animate('.1s ease-in',)
-      ]),
-    ])
-  ],
   templateUrl: './image-viewer.component.html',
   styleUrls: ['./image-viewer.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
