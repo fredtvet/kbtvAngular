2767bb11e4263ed26ee0998efcdfe173705502ab
Fredrik Tvete <fretve@hotmail.com>
2020-10-05 00:38:59 +0200

    add null check

--- src/app/shared-app/helpers/object/map-objects-to-weekdays.helper.ts
+++ src/app/shared-app/helpers/object/map-objects-to-weekdays.helper.ts
@@ -1,8 +1,9 @@
 export function _mapObjectsToWeekdays<TObject>(objects: TObject[], dateProp: string): { [key: number]: TObject }{
-    const result: { [key: number]: TObject } = {};
-    for(let i = 0; i < objects.length; i++){
-      const obj = objects[i];
-      result[new Date(obj[dateProp]).getDay() || 7] = obj;
-    }
-    return result;
+  if(!objects) return;
+  const result: { [key: number]: TObject } = {};
+  for(let i = 0; i < objects.length; i++){
+    const obj = objects[i];
+    result[new Date(obj[dateProp]).getDay() || 7] = obj;
+  }
+  return result;
 }
