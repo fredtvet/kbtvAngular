5b7f674e39581da884df04a4100fff323b8b3849
Fredrik Tvete <fretve@hotmail.com>
2020-10-06 20:16:08 +0200

    show all use last day of year to fix bug

--- src/app/shared-app/helpers/datetime/get-range-by-date-range-preset.helper.ts
+++ src/app/shared-app/helpers/datetime/get-range-by-date-range-preset.helper.ts
@@ -1,4 +1,5 @@
 import { DateRangePresets } from "src/app/shared-app/enums/date-range-presets.enum";
+import { _getLastDayOfYear } from './get-last-day-of-year.helper';
 import { _getMonthRange } from './get-month-range.helper';
 import { _getWeekRange } from './get-week-range.helper';
 import { _getYearRange } from './get-year-range.helper';
@@ -12,7 +13,7 @@ export function _getRangeByDateRangePreset(preset: DateRangePresets): Date[] {
       case DateRangePresets.CurrentYear:
         return _getYearRange();
       case DateRangePresets.ShowAll:
-        return [new Date(0), new Date()];
+        return [new Date(0), _getLastDayOfYear(new Date())];
       default: return undefined;
     }
   }
