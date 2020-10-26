67a17c5c2957aaf5c568f94d947a392e727f4b7b
Fredrik Tvete <fretve@hotmail.com>
2020-10-26 20:09:03 +0100

    remove online/offline notification

--- src/app/core/services/startup.service.ts
+++ src/app/core/services/startup.service.ts
@@ -1,7 +1,4 @@
 import { Injectable } from '@angular/core';
-import { skip } from 'rxjs/operators';
-import { NotificationService, NotificationType } from 'src/app/shared-app/notification';
-import { DeviceInfoService } from './device-info.service';
 import { IconService } from './icon.service';
 import { PersistanceStore } from './persistance/persistance.store';
 import { SyncStore } from './sync/sync.store';
@@ -12,19 +9,10 @@ export class StartupService {
   constructor(
     syncStore: SyncStore,
     persistanceStore: PersistanceStore,
-    deviceInfoService: DeviceInfoService,
     iconService: IconService,
-    notificationService: NotificationService
   ) { 
     persistanceStore.init();
-
     syncStore.init();
-
     iconService.registerIcons();
-    
-    deviceInfoService.isOnline$.pipe(skip(1)).subscribe(isOnline => {
-      if(isOnline) notificationService.notify({title: 'Du er tilkoblet internett igjen!', type: NotificationType.Success})
-      else notificationService.notify({title: 'Du er nå i frakoblet modus. Noen funksjoner er deaktivert.', type: NotificationType.Warning})
-    });  
   }
 }
