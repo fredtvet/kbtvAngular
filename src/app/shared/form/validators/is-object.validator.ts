9c526342ad31fed5f124fb576f626b17018c009d
Fredrik Tvete <fretve@hotmail.com>
2020-10-27 20:11:04 +0100

    fix isobject validator

--- src/app/shared/form/validators/is-object.validator.ts
+++ src/app/shared/form/validators/is-object.validator.ts
@@ -2,7 +2,7 @@ import { ValidatorFn, AbstractControl } from '@angular/forms';
 
 export function isObjectValidator(): ValidatorFn{ 
     return (control: AbstractControl): {[key: string]: any} | null => {
-      let invalid = !(control.value instanceof Object);
+      let invalid = !(!control.value || control.value instanceof Object);
       return invalid ? {'isobject': {value: control.value}} : null;
     };
   }
