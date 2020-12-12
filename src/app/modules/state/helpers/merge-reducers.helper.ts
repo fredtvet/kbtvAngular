b3ad9d2debec5bc2e7535066ca17b9c272893fb5
Fredrik Tvete <fretve@hotmail.com>
2020-12-12 14:48:35 +0100

    merge reducers fix null errors

--- src/app/modules/state/helpers/merge-reducers.helper.ts
+++ src/app/modules/state/helpers/merge-reducers.helper.ts
@@ -7,13 +7,14 @@ import { _getReducersStateProps } from './get-reducers-state-props.helper';
 export function _mergeReducers(reducers: Reducer<any, StateAction>[], action: StateAction): Reducer<any, StateAction> {
     return <Reducer<any, StateAction>>{action,
         reducerFn: (state: any, action: any): any => {
-            let newState: any = {}; 
+            let newState: any; 
             for(const reducer of reducers) {
                 const inputAction = _cloneInstance(action, !reducer.noDeepCloneAction);
                 const inputState = reducer.noDeepCloneState ? {...state} : _deepClone(state);
                 const reducerState = reducer.reducerFn(inputState, inputAction);
+                if(!reducerState) continue;
                 state = {...state, ...reducerState} //merge total state for next reducer
-                newState = {...newState, ...reducerState} //Store all new state to prevent emitting entire state
+                newState = {...newState || {}, ...reducerState} //Store all new state to prevent emitting entire state
             }
             return newState;
         },
