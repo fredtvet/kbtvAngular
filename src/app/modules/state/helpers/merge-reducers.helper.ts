9ad8977abb9075a5be997da7cb7ed8468e512025
Fredrik Tvete <fretve@hotmail.com>
2020-12-12 14:42:44 +0100

    merge reducers only return modified state

--- src/app/modules/state/helpers/merge-reducers.helper.ts
+++ src/app/modules/state/helpers/merge-reducers.helper.ts
@@ -7,12 +7,15 @@ import { _getReducersStateProps } from './get-reducers-state-props.helper';
 export function _mergeReducers(reducers: Reducer<any, StateAction>[], action: StateAction): Reducer<any, StateAction> {
     return <Reducer<any, StateAction>>{action,
         reducerFn: (state: any, action: any): any => {
+            let newState: any = {}; 
             for(const reducer of reducers) {
                 const inputAction = _cloneInstance(action, !reducer.noDeepCloneAction);
                 const inputState = reducer.noDeepCloneState ? {...state} : _deepClone(state);
-                state = {...state, ...reducer.reducerFn(inputState, inputAction)}
+                const reducerState = reducer.reducerFn(inputState, inputAction);
+                state = {...state, ...reducerState} //merge total state for next reducer
+                newState = {...newState, ...reducerState} //Store all new state to prevent emitting entire state
             }
-            return state;
+            return newState;
         },
         stateProperties: (action: any) => _getReducersStateProps(reducers, action)
     }
