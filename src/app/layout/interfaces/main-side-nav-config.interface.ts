0c4af407b2eec2544241a3a8176ae9a128c70107
Fredrik Tvete <fretve@hotmail.com>
2020-12-21 18:48:02 +0100

    add sync timestamp on main side nav header

--- src/app/layout/interfaces/main-side-nav-config.interface.ts
+++ src/app/layout/interfaces/main-side-nav-config.interface.ts
@@ -4,6 +4,7 @@ import { AppButton } from '@shared-app/interfaces/app-button.interface';
 
 export interface MainSideNavConfig{
     user: Maybe<Immutable<User>>;
+    syncTimestamp: number;
     isOnline: boolean;
     navigations: AppButton[];
 }
--- src/app/layout/main-nav.service.ts
+++ src/app/layout/main-nav.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
+import { User } from '@core/models';
+import { DeviceInfoService } from '@core/services/device-info.service';
+import { StateCurrentUser } from '@core/state/global-state.interfaces';
+import { Store } from '@state/store';
+import { StateSyncTimestamp } from '@sync/interfaces';
 import { combineLatest, Observable, Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { AuthService } from '@core/services/auth/auth.service';
-import { DeviceInfoService } from '@core/services/device-info.service';
 import { BottomNavNavigations } from './bottom-nav-navigations';
 import { MainNavConfig } from './interfaces/main-nav-config.interface';
 import { SideNavNavigations } from './side-nav-navigations';
@@ -15,20 +18,21 @@ export class MainNavService {
 
   config$: Observable<MainNavConfig> = combineLatest([
     this.deviceInfoService.isXs$, 
-    this.authService.currentUser$,
+    this.store.selectProperty$<User>("currentUser"),
+    this.store.selectProperty$<number>("syncTimestamp"),
     this.deviceInfoService.isOnline$, 
     ]).pipe(
-      map(([isXs, user, isOnline]) => {
+      map(([isXs, user, syncTimestamp, isOnline]) => {
         return {
           isXs, 
-          sideNavConfig: {user, isOnline, navigations: SideNavNavigations},
+          sideNavConfig: {user, syncTimestamp, isOnline, navigations: SideNavNavigations},
           bottomNavigations: BottomNavNavigations
         } 
       }),
   );
 
   constructor(
-    private authService: AuthService,
+    private store: Store<StateCurrentUser & StateSyncTimestamp>,
     private deviceInfoService: DeviceInfoService) {}
 
   toggleDrawer(): void {
