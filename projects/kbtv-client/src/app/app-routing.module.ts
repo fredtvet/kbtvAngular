8761e81ddf870c3bc58c7181232d86615ae2e005
Fredrik Tvete <fretve@hotmail.com>
2020-12-27 22:56:04 +0100

    update angular

--- projects/kbtv-client/src/app/app-routing.module.ts
+++ projects/kbtv-client/src/app/app-routing.module.ts
@@ -48,8 +48,9 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes,  {
-      preloadingStrategy: RolePreloadService
-  })],
+    preloadingStrategy: RolePreloadService,
+    relativeLinkResolution: 'legacy'
+})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
