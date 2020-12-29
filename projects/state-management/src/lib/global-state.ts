ce406283d58f321306b38323ffbe4ec8a75f3d88
Fredrik Tvete <fretve@hotmail.com>
2020-12-29 22:05:32 +0100

    fix compile errors

---
+++ projects/state-management/src/lib/global-state.ts
@@ -0,0 +1,3 @@
+import { StateBase } from "./state-base";
+
+export const State = new StateBase();
--- projects/state-management/src/lib/store.ts
+++ projects/state-management/src/lib/store.ts
@@ -1,13 +1,11 @@
 import { Inject, Injectable, Optional, Self, SkipSelf } from '@angular/core';
-import { UnknownState } from 'global-types';
+import { Maybe, UnknownState } from 'global-types';
 import { ActionDispatcher } from './action-dispatcher';
 import { STORE_DEFAULT_STATE, STORE_META_REDUCERS, STORE_REDUCERS, STORE_SETTINGS } from './constants/injection-tokens.const';
+import { State } from './global-state';
 import { MetaReducer, Reducer, StoreSettings } from './interfaces';
-import { StateBase } from './state-base';
 import { StateAction } from './state.action';
 import { StoreBase } from './store-base';
-
-const State = new StateBase();
 
 @Injectable()
 export class Store<TState> extends StoreBase<TState> {
@@ -17,14 +15,14 @@ export class Store<TState> extends StoreBase<TState> {
         @Self() actionDispatcher: ActionDispatcher,     
         @Self() @Optional() @Inject(STORE_REDUCERS) reducers: Reducer<unknown, StateAction>[],
         @Self() @Optional() @Inject(STORE_META_REDUCERS) metaReducers: MetaReducer<unknown, StateAction>[],
-        @Self() @Optional() @Inject(STORE_DEFAULT_STATE) defaultState: Partial<TState>,
+        @Self() @Optional() @Inject(STORE_DEFAULT_STATE) defaultState: UnknownState,
         @Optional() @Inject(STORE_SETTINGS) storeSettings: StoreSettings,
     ) { 
         super(State, hostStore, actionDispatcher, reducers, metaReducers, storeSettings); 
         if(defaultState) Store.setDefaultState({...defaultState});
     }
 
-    static defaultState: Object;
+    static defaultState: Maybe<Object> = null;
 
     static setDefaultState(defaultState: UnknownState): void {
         Store.defaultState = {...Store.defaultState, ...defaultState}; //accumulate default state from feature modules to keep track
