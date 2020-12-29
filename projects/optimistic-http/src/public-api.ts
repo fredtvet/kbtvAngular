2ac583022746116681ff34ad8133409d57f8aaa6
Fredrik Tvete <fretve@hotmail.com>
2020-12-29 16:09:36 +0100

    cleanup optimistic http lib exposure

--- projects/optimistic-http/src/public-api.ts
+++ projects/optimistic-http/src/public-api.ts
@@ -10,6 +10,3 @@ export * from './lib/optimistic-http.module';
 export * from './lib/state/optimistic-http.action'
 export * from './lib/state/http-queue-shift.action'
 export * from './lib/state/http-error/http-error.action'
-export * from './lib/state/http-success/http-success.action'
-export * from './lib/state/http-queue-push/http-queue-push.action'
-export * from './lib/state/dispatch-http/dispatch-http.action'
