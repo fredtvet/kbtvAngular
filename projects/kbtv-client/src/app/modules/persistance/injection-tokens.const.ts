2d3b519da085aeb8e6bbf149f4508f91b9246096
Fredrik Tvete <fretve@hotmail.com>
2020-12-29 15:40:39 +0100

    remove unused persistance module

--- projects/kbtv-client/src/app/modules/persistance/injection-tokens.const.ts
+++
@@ -1,5 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import { PersistanceConfig } from './interfaces';
-
-export const PERSISTANCE_CONFIG = new InjectionToken<PersistanceConfig<unknown>>('PERSISTANCE_CONFIG');
-
