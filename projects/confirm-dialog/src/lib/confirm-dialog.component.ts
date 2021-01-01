5ff5909c548700f03c5450af0ea1be2af24c3fa4
Fredrik Tvete <fretve@hotmail.com>
2021-01-01 17:45:06 +0100

    refactor confirm dialog html & remove global style dependency

--- projects/confirm-dialog/src/lib/confirm-dialog.component.ts
+++ projects/confirm-dialog/src/lib/confirm-dialog.component.ts
@@ -5,24 +5,14 @@ import { ConfirmDialogConfig } from './confirm-dialog-config.interface';
 @Component({
   selector: 'lib-confirm-dialog',
   template: `
-  <mat-card>
-
-    <mat-card-header *ngIf="config.title">
-      <mat-card-title>
-        {{ config.title }}
-      </mat-card-title>
-    </mat-card-header>
-
-    <mat-card-content *ngIf="config.message">
-      <p>{{ config.message || 'Vennligst bekreft operasjonen.' }}</p>
-    </mat-card-content>
-
-    <mat-card-actions fxLayout="row" fxLayoutAlign="end center">
-      <button mat-button (click)="onNoClick()">{{ config.discardText || 'Avbryt'}}</button>
-      <button mat-button  [mat-dialog-close]="true" cdkFocusInitial>{{ config.confirmText }}</button>
-    </mat-card-actions>
-
-  </mat-card>
+  <h2 *ngIf="config.title" mat-dialog-title>{{ config.title }}</h2>
+  <div mat-dialog-content *ngIf="config.message">
+    {{ config.message || 'Vennligst bekreft operasjonen.' }}
+  </div>
+  <div mat-dialog-actions align="end">
+    <button mat-button (click)="onNoClick()">{{ config.discardText || 'Avbryt'}}</button>
+    <button mat-button  [mat-dialog-close]="true" cdkFocusInitial>{{ config.confirmText }}</button>
+  </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
