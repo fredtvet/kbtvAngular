da0df66d5a688b6aea4900af90288397c88f9b2e
Fredrik Tvete <fretve@hotmail.com>
2021-01-05 16:22:30 +0100

    fix get-date-of-week.helper

--- projects/date-time-helpers/src/lib/get-date-of-week.helper.ts
+++ projects/date-time-helpers/src/lib/get-date-of-week.helper.ts
@@ -1,4 +1,10 @@
-export function _getDateOfWeek(weekNr: number, year: number) {
-    var d = (1 + (weekNr - 1) * 7); // 1st of January + 7 days for each week
-    return new Date(year, 0, d);
+export function _getDateOfWeek(w: number, y: number) {
+    var simple = new Date(y, 0, 1 + (w - 1) * 7);
+    var dow = simple.getDay();
+    var ISOweekStart = simple;
+    if (dow <= 4)
+        ISOweekStart.setDate(simple.getDate() - simple.getDay() + 1);
+    else
+        ISOweekStart.setDate(simple.getDate() + 8 - simple.getDay());
+    return ISOweekStart;
 }
