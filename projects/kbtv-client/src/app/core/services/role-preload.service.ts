77009642e7d967d2f1c4f4b36dd14f7e36ead114
Fredrik Tvete <fretve@hotmail.com>
2021-01-05 17:51:27 +0100

    typesafe role-preload service

--- projects/kbtv-client/src/app/core/services/role-preload.service.ts
+++ projects/kbtv-client/src/app/core/services/role-preload.service.ts
@@ -1,22 +1,23 @@
 import { Injectable } from '@angular/core';
-import { PreloadingStrategy, Route } from '@angular/router';
+import { PreloadingStrategy } from '@angular/router';
+import { CustomRoute } from '@shared-app/interfaces/custom-route.interface';
 import { Maybe } from 'global-types';
 import { EMPTY, Observable, timer } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
-import { AuthService } from 'state-auth';
+import { AuthRouteData, AuthService } from 'state-auth';
 
 @Injectable({ providedIn: 'root' })
 export class RolePreloadService implements PreloadingStrategy {  
 
     constructor(private authService: AuthService){ }
 
-    preload(route: Route, load: () => Observable<unknown>): Observable<unknown> {
+    preload(route: CustomRoute<AuthRouteData>, load: () => Observable<unknown>): Observable<unknown> {
       const user = this.authService.getCurrentUser();
       if(!this.preloadCheck(route, user?.role)) return EMPTY;
       return timer(2000).pipe(switchMap(x => load()))
     }
   
-    private preloadCheck(route: Route, role: Maybe<string>): boolean {
+    private preloadCheck(route: CustomRoute<AuthRouteData>, role: Maybe<string>): boolean {
       return (
         route.data != null && role != null &&
         (!route.data['allowedRoles'] || (route.data['allowedRoles'].indexOf(role) !== -1))
