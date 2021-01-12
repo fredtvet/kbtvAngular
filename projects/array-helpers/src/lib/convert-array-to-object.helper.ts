43a9ba32629027cd7654613555d47b7c69a3240a
Fredrik Tvete <fretve@hotmail.com>
2021-01-12 19:52:42 +0100

    allow functions to specity key in arrayToObject converter

--- projects/array-helpers/src/lib/convert-array-to-object.helper.ts
+++ projects/array-helpers/src/lib/convert-array-to-object.helper.ts
@@ -4,12 +4,12 @@ type Response<T> = { [key: string]: Maybe<Immutable<T>> }
 /**
  * Convert an array to a key/value object
  * @param array 
- * @param key If property on the array value that will be used as the key for its value. If null, the value is used as key.
+ * @param key An item property or function used as key in the object. If null, the value is used as key.
  * @returns An object containing keys with accociated values
  */
 export function _convertArrayToObject<T>(
   array: Maybe<ImmutableArray<T>>, 
-  key?: Prop<Immutable<T>>   
+  key?: Prop<Immutable<T>> | ((t: Immutable<T>) => string)  
 ): Response<T> {
     if(!array?.length) return {};
     const result: Response<T> = {}; 
@@ -19,6 +19,8 @@ export function _convertArrayToObject<T>(
 
     if(typeof key === "string")
       setter = (entity: Immutable<T>, result: Response<T>) => result[<string> entity[key]] = entity
+    else if(typeof key === "function")
+      setter = (entity: Immutable<T>, result: Response<T>) => result[key(entity)] = entity
 
     for(var i = 0; i < array.length; i++) setter(array[i], result);
 
