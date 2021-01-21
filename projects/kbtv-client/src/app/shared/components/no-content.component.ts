7453f015f9371269a6bca19cd23105fccd7f0f7c
Fredrik Tvete <fretve@hotmail.com>
2021-01-21 12:48:33 +0100

    update no content component prefill some button info

--- projects/kbtv-client/src/app/shared/components/no-content.component.ts
+++ projects/kbtv-client/src/app/shared/components/no-content.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ButtonTypes } from '@shared-app/enums/button-types.enum';
 import { AppButton } from '@shared-app/interfaces/app-button.interface';
 
 @Component({
@@ -6,12 +7,23 @@ import { AppButton } from '@shared-app/interfaces/app-button.interface';
   template: `
     <span class="text-center w-100">
         <p>{{ text }}</p>
-        <app-button *ngIf="button" [config]="button" class="bounceIn"></app-button>
+        <app-button *ngIf="button" [config]="button" class="bounceIn" style="display:block"></app-button>
     </span>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NoContentComponent{
   @Input() text: string;
-  @Input() button: AppButton;
+
+  button: AppButton;
+
+  @Input('buttonInfo') 
+  set config(value: Pick<AppButton, "text" | "icon" | "callback" | "params">) {
+    this.button = {
+        ...value,
+        type: ButtonTypes.Flat,
+        color: 'accent',
+    }
+  } 
+
 }
