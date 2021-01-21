a7d15be39450cb3cb194cc64a990100988a872a7
Fredrik Tvete <fretve@hotmail.com>
2021-01-21 13:00:40 +0100

    add production toggle for iphone detection method

--- projects/kbtv-client/src/app/core/services/device-info.service.ts
+++ projects/kbtv-client/src/app/core/services/device-info.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { fromEvent, Observable, merge, combineLatest, of } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { environment } from 'src/environments/environment';
 
 export interface DeviceInfo {
   isOnline: boolean;
@@ -11,7 +12,11 @@ export interface DeviceInfo {
 @Injectable({providedIn: 'root'})
 export class DeviceInfoService {
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver) {
+    //use navigator user agent for debugging with dev tools.
+    const iphoneId = environment.production ? navigator.platform : navigator.userAgent;
+    this.isIphone = /(iPad|iPhone)/g.test(iphoneId); 
+  }
 
   get isOnline(){ return navigator.onLine };
 
@@ -25,7 +30,7 @@ export class DeviceInfoService {
     // })
   );
 
-  isIphone = /(iPad|iPhone)/g.test(navigator.userAgent);
+  isIphone: boolean;
 
   isXs$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.XSmall)
     .pipe(
