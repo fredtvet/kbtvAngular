07d788f1325765b8d3e9addd577da6d4c6221af6
Fredrik Tvete <fretve@hotmail.com>
2021-02-02 15:59:09 +0100

    add bottom action bar to timesheet statistic

--- projects/kbtv-client/src/app/feature-modules/timesheet-modules/timesheet-statistic/timesheet-statistic/timesheet-statistic.component.ts
+++ projects/kbtv-client/src/app/feature-modules/timesheet-modules/timesheet-statistic/timesheet-statistic/timesheet-statistic.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
 import { Timesheet } from '@core/models';
 import { ChipsFactoryService } from '@core/services/ui/chips-factory.service';
 import { _getSetPropCount } from '@shared-app/helpers/object/get-set-prop-count.helper';
+import { AppButton } from '@shared-app/interfaces/app-button.interface';
 import { AppChip } from '@shared-app/interfaces/app-chip.interface';
 import { AgGridConfig } from '@shared/components/abstracts/ag-grid-config.interface';
 import { MainTopNavConfig } from '@shared/components/main-top-nav-bar/main-top-nav.config';
@@ -12,14 +13,14 @@ import { FormService } from 'form-sheet';
 import { Immutable, Maybe, Prop } from 'global-types';
 import { combineLatest, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { TimesheetSummary } from '../../shared-timesheet/interfaces';
-import { TimesheetCriteriaChipOptions } from '../../shared-timesheet/timesheet-filter/timesheet-criteria-chip-options.const';
-import { TimesheetCriteria } from '../../shared-timesheet/timesheet-filter/timesheet-criteria.interface';
+import { TimesheetSummary } from '@shared-timesheet/interfaces';
+import { TimesheetCriteriaChipOptions } from '@shared-timesheet/timesheet-filter/timesheet-criteria-chip-options.const';
+import { TimesheetCriteria } from '@shared-timesheet/timesheet-filter/timesheet-criteria.interface';
 import { TimesheetStatisticFacade } from '../timesheet-statistic.facade';
 import { TimesheetStatisticProviders } from './timesheet-statistic-providers.const';
 import { TimesheetStatisticTableComponent } from './timesheet-statistic-table/timesheet-statistic-table.component';
 
-interface NavViewModel { groupByChips: AppChip[], criteriaChips: AppChip[],  navConfig: MainTopNavConfig }
+interface ViewModel { groupByChips: AppChip[], criteriaChips: AppChip[], tableConfig: AgGridConfig<TimesheetSummary | Timesheet> }
 
 @Component({
   selector: 'app-timesheet-statistic',
@@ -30,31 +31,34 @@ interface NavViewModel { groupByChips: AppChip[], criteriaChips: AppChip[],  nav
 export class TimesheetStatisticComponent {
   @ViewChild('statTable') statTable: TimesheetStatisticTableComponent;
 
-  private partialVm$: Observable<Partial<NavViewModel>> = this.facade.criteria$.pipe(map(criteria => { 
-    const activeCriteriaCount = criteria ? _getSetPropCount(criteria, {dateRangePreset: null}) : 0
-    return {
-      criteriaChips: this.getCriteriaChips(criteria || {}, activeCriteriaCount), 
-      navConfig: this.getNavConfig(activeCriteriaCount)
-    }
-  }))
-
-  navVm$: Observable<NavViewModel> = combineLatest([
-    this.facade.groupBy$.pipe(map(x => this.getGroupByChips(x))),
-    this.partialVm$
-  ]).pipe(
-    map(([groupByChips, navVm]) => {
-      navVm.groupByChips = groupByChips; 
-      return <NavViewModel> navVm
-    }), 
+  private criteriChips$: Observable<AppChip[]> = this.facade.criteria$.pipe(
+    map(criteria => { 
+      const activeCriteriaCount = criteria ? _getSetPropCount(criteria, {dateRangePreset: null}) : 0
+      return  this.getCriteriaChips(criteria || {}, activeCriteriaCount)   
+    })
   )
-  
-  tableConfig$: Observable<AgGridConfig<TimesheetSummary | Timesheet>> = this.facade.tableConfig$;
 
+  vm$: Observable<ViewModel> = combineLatest([
+    this.facade.groupBy$.pipe(map(x => this.getGroupByChips(x))),
+    this.criteriChips$,
+    this.facade.tableConfig$
+  ]).pipe(map(([groupByChips, criteriaChips, tableConfig]) => { 
+    return {groupByChips, criteriaChips, tableConfig} 
+  }))
+  
+  navConfig: MainTopNavConfig = {title:  'Timestatistikk'};
+  bottomActions: AppButton[];
+  
   constructor( 
     private facade: TimesheetStatisticFacade,
     private formService: FormService,
     private chipsFactory: ChipsFactoryService
-  ) { }
+  ) { 
+    this.bottomActions =  [
+      {icon: "filter_list", callback: this.openTimesheetFilter},
+      {icon: "cloud_download", callback: this.exportAsCsv}     
+    ]
+  }
 
   private openTimesheetFilter = (): void => {
     this.formService.open<TimesheetCriteria, TimesheetCriteriaFormState>({
@@ -91,16 +95,6 @@ export class TimesheetStatisticComponent {
   }
 
   private addGroupBy = (groupBy: GroupByPeriod) => this.facade.updateGroupBy(groupBy);
-
-  private getNavConfig(activeCriteriaCount: number): MainTopNavConfig {
-    return { title:  "Timestatistikk",
-      buttons: [
-        {icon: "filter_list", color: activeCriteriaCount && activeCriteriaCount > 0 ? "accent" : undefined, 
-          callback: this.openTimesheetFilter},
-        {icon: "cloud_download", callback: this.exportAsCsv}     
-      ]
-    }
-  }
 
   private getGroupByChips(groupBy: Maybe<GroupByPeriod>):  AppChip[] {
     return this.chipsFactory.createEnumSelectionChips(GroupByPeriod, groupBy, this.addGroupBy);
