ed159f2680d0f517b2d1fdacd6eb1c3d9239a4e4
Fredrik Tvete <fretve@hotmail.com>
2021-02-04 15:05:15 +0100

    fix bug when adding to empty array

--- projects/array-helpers/src/lib/add.helper.ts
+++ projects/array-helpers/src/lib/add.helper.ts
@@ -6,7 +6,7 @@ import { Immutable, ImmutableArray, Maybe } from 'global-types';
  * @param value 
  */
 export function _add<T>(array: Maybe<ImmutableArray<T>>, value: Immutable<T>): Immutable<T>[]{
-    if(!array?.length) return [];
+    if(!array?.length) return [value];
     let arr = array.slice();
     arr.unshift(value);
     return arr;
