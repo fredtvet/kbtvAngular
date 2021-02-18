58ae267ec25e33ddbc8a56feb9f5ee8e95344c9d
Fredrik Tvete <fretve@hotmail.com>
2021-02-18 23:24:23 +0100

    add or update add to start of array

--- projects/array-helpers/src/lib/add-or-update-range.helper.ts
+++ projects/array-helpers/src/lib/add-or-update-range.helper.ts
@@ -4,33 +4,32 @@ import { _convertArrayToObject } from './convert-array-to-object.helper';
 /**
  * Merge two arrays of objects, updating any objects that overlap
  * @param originals The original array of objects that may be overriden
- * @param newEntities The new array of objects that should be added 
+ * @param values The new array of objects that should be added 
  * @param idProp A property on the object that uniquely identifies it
  */
 export function _addOrUpdateRange<T>(
     originals: Maybe<ImmutableArray<T>>, 
-    newEntities: Maybe<ImmutableArray<T>>, 
+    values: Maybe<ImmutableArray<T>>, 
     idProp: Prop<Immutable<T>>): Immutable<T>[]{   
 
-    if(!newEntities?.length) return originals?.slice() || []; //If no entities, just return current val
-    if(!originals?.length) return newEntities.slice(); //If initial array empty, just return empty array
+    if(!values?.length) return originals?.slice() || []; //If no entities, just return current val
+    if(!originals?.length) return values.slice(); //If initial array empty, just return empty array
 
     let originalsObj = _convertArrayToObject(originals, idProp);
-
-    for(let i = 0; i < newEntities.length; i++){  
-      let newObj = newEntities[i];
-      const itemKey = idProp ? newObj[idProp] : newObj; //Use idProp as key else obj
-      let existingObj = originalsObj[<string> itemKey]; //Grab existing obj
-      if(existingObj) originalsObj[<string> itemKey] = {...existingObj, ...newObj}; //If obj exist, update it with new obj
-      else originalsObj[<string> itemKey] = newObj;    
+    const newEntities: Immutable<T>[] = [];
+    for(let i = 0; i < values.length; i++){  
+      let newObj = values[i];
+      const itemKey = <string> (idProp ? newObj[idProp] : newObj); //Ungse idProp as key else obj
+      let existingObj = originalsObj[itemKey]; //Grab existing obj
+      if(existingObj) originalsObj[itemKey] = {...existingObj, ...newObj}; //If obj exist, update it with new obj
+      else newEntities.push(newObj);    
     } 
 
-    let result: Immutable<T>[] = [];
     let keys = Object.keys(originalsObj);
     
     for(let i = 0; i < keys.length;i++){
-      result.push(<Immutable<T>> originalsObj[keys[i]]);
+      newEntities.push(<Immutable<T>> originalsObj[keys[i]]);
     }
 
-    return result;
+    return newEntities;
 }
