88ac2bcf3f741c627af02e885e6ea5cdff35e8e7
Fredrik Tvete <fretve@hotmail.com>
2021-02-21 20:32:35 +0100

    tap to click on note to fix bug & add null check to auth fn

--- projects/kbtv-client/src/app/shared-app/helpers/get-authenticated-resources.helper.ts
+++ projects/kbtv-client/src/app/shared-app/helpers/get-authenticated-resources.helper.ts
@@ -1,6 +1,7 @@
 import { User } from "@core/models";
 
 export function _getAuthenticatedResources<T extends {allowedRoles?: string[]}>(resources: T[], user: User): T[] {
+    if(!resources) return [];
     const authResources: T[] = [];
     for(const resource of resources){
         if(!resource.allowedRoles || resource.allowedRoles.indexOf(user.role) !== -1)
