c9b533bb1c079541e189a95baffd77c5c457ccc2
Fredrik Tvete <fretve@hotmail.com>
2021-02-24 12:01:20 +0100

    add margintop to no content

--- projects/kbtv-client/src/app/shared/components/no-content.component.ts
+++ projects/kbtv-client/src/app/shared/components/no-content.component.ts
@@ -5,8 +5,9 @@ import { AppButton } from '@shared/components/app-button/app-button.interface';
 @Component({
   selector: 'app-no-content',
   template: `
-    <div class="text-center w-100">
-        <mat-icon *ngIf="icon" style="width:100%;margin-bottom:16px;font-size:50px;">{{ icon }}</mat-icon>
+    <style>mat-icon{ width:100%;font-size:50px; }</style>
+    <div class="text-center w-100 mt-6">
+        <mat-icon class="mb-4" *ngIf="icon">{{ icon }}</mat-icon>
         <div class="mt-4"><ng-content></ng-content></div>
         <app-button *ngIf="button" [config]="button" class="fadeIn mt-4" style="display:block"></app-button>
     </div>
