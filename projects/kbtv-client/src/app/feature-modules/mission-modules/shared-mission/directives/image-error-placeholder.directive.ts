4cc9717a4a90433328255d6f12d34a8536e3dec1
Fredrik Tvete <fretve@hotmail.com>
2021-03-03 11:09:50 +0100

    fix bug when notfound img is also missing

--- projects/kbtv-client/src/app/feature-modules/mission-modules/shared-mission/directives/image-error-placeholder.directive.ts
+++ projects/kbtv-client/src/app/feature-modules/mission-modules/shared-mission/directives/image-error-placeholder.directive.ts
@@ -3,10 +3,14 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
 @Directive({selector: '[appImageErrorPlaceholder]'})
 export class ImageErrorPlaceholderDirective {
 
-  constructor(private el: ElementRef) {}
+    private hasSetPlaceholder: boolean;
 
-  @HostListener('error') onError(): void {
-    this.el.nativeElement.src = "/assets/notfound.png?"
-  }
+    constructor(private el: ElementRef) {}
+
+    @HostListener('error') onError(): void {
+        if(this.hasSetPlaceholder) return;
+        this.hasSetPlaceholder = true; 
+        this.el.nativeElement.src =  "/assets/notfound.png?";     
+    }
 
 }
