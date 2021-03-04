38d4d3594dd66a1ea2aeda96c9bc128e0b8c8968
Fredrik Tvete <fretve@hotmail.com>
2021-03-04 21:26:35 +0100

    use different default sync config for desktop

--- projects/kbtv-client/src/app/core/configurations/default-state.const.ts
+++ projects/kbtv-client/src/app/core/configurations/default-state.const.ts
@@ -1,9 +1,15 @@
 import { StateSyncConfig } from 'state-sync';
 import { _getDateYearsAgo } from 'date-time-helpers'
 
+var isMobile = /Android|webOS|iPhone|iPad|Mac|Macintosh|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
 export const DefaultState: StateSyncConfig = {
-    syncConfig: {
-        refreshTime: 60*15, 
-        initialTimestamp: _getDateYearsAgo(2).getTime(),
-    }
+    syncConfig: 
+        isMobile ? {
+            refreshTime: 60*15, 
+            initialTimestamp: _getDateYearsAgo(2).getTime(),
+        } : {
+            refreshTime: 60*5, 
+            initialTimestamp: _getDateYearsAgo(4).getTime(),
+        }
 }
