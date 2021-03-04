a3426230872c6773cceb3145a855825ac76f07ce
Fredrik Tvete <fretve@hotmail.com>
2021-03-04 21:21:19 +0100

    update default state

--- projects/kbtv-client/src/app/core/configurations/default-state.const.ts
+++ projects/kbtv-client/src/app/core/configurations/default-state.const.ts
@@ -3,7 +3,7 @@ import { _getDateYearsAgo } from 'date-time-helpers'
 
 export const DefaultState: StateSyncConfig = {
     syncConfig: {
-        refreshTime: 60*30, 
-        initialTimestamp: _getDateYearsAgo(4).getTime(),
+        refreshTime: 60*15, 
+        initialTimestamp: _getDateYearsAgo(2).getTime(),
     }
 }
