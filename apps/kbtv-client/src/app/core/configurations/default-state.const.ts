4b40108c49a8040e954085f858c2ea6346580b41
Fredrik Tvete <fretve@hotmail.com>
2021-04-06 17:22:16 +0200

    clean up styles

--- apps/kbtv-client/src/app/core/configurations/default-state.const.ts
+++ apps/kbtv-client/src/app/core/configurations/default-state.const.ts
@@ -1,5 +1,5 @@
 import { StateSyncConfig } from 'state-sync';
-import { _getDateYearsAgo } from 'date-time-helpers'
+import { _getDateYearsAgo, _getFirstDayOfMonth } from 'date-time-helpers'
 
 var isMobile = /Android|webOS|iPhone|iPad|Mac|Macintosh|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
@@ -7,9 +7,9 @@ export const DefaultState: StateSyncConfig = {
     syncConfig: 
         isMobile ? {
             refreshTime: 60*15, 
-            initialTimestamp: _getDateYearsAgo(2).getTime(),
+            initialTimestamp:_getFirstDayOfMonth(_getDateYearsAgo(2)).getTime(),
         } : {
             refreshTime: 60*5, 
-            initialTimestamp: _getDateYearsAgo(4).getTime(),
+            initialTimestamp: _getFirstDayOfMonth(_getDateYearsAgo(4)).getTime(),
         }
 }
--- apps/kbtv-client/src/app/shared-app/components/no-content.component.ts
+++ apps/kbtv-client/src/app/shared-app/components/no-content.component.ts
@@ -4,7 +4,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
   selector: 'app-no-content',
   template: `
     <style>mat-icon{ width:100%;font-size:50px; }</style>
-    <div class="text-center w-100 mt-6">
+    <div class="w-100 mt-6" style="text-align: center">
         <mat-icon class="mb-4" *ngIf="icon">{{ icon }}</mat-icon>
         <div class="mt-4"><ng-content></ng-content></div>
     </div>
