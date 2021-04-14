bc31fb5e6330723aa81b47fba4780b22a0bae73c
Fredrik Tvete <fretve@hotmail.com>
2021-04-14 09:57:08 +0200

    remove optimistic http for update user pw

--- apps/kbtv-client/src/app/feature-modules/users/update-user-password.http.effect.ts
+++ apps/kbtv-client/src/app/feature-modules/users/update-user-password.http.effect.ts
@@ -1,28 +1,21 @@
 import { UpdateUserPasswordAction } from '@actions/user-actions';
 import { Injectable } from '@angular/core';
 import { ApiUrl } from '@core/api-url.enum';
-import { OptimisticHttpAction } from 'optimistic-http';
+import { ApiService } from '@core/services/api.service';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { mergeMap } from 'rxjs/operators';
 import { DispatchedAction, Effect, listenTo } from 'state-management';
 
 @Injectable()
 export class UpdateUserPasswordHttpEffect implements Effect<UpdateUserPasswordAction> {
 
-    constructor(){}
+    constructor(private apiService: ApiService){}
 
-    handle$(actions$: Observable<DispatchedAction<UpdateUserPasswordAction>>): Observable<OptimisticHttpAction> {
+    handle$(actions$: Observable<DispatchedAction<UpdateUserPasswordAction>>): Observable<void> {
         return actions$.pipe(
             listenTo([UpdateUserPasswordAction]),
-            map(x => <OptimisticHttpAction>{
-                type: OptimisticHttpAction, propagate: true,
-                request: {
-                    apiUrl: `${ApiUrl.Users}/${x.action.userName}/NewPassword`,
-                    method: "PUT", 
-                    body: x.action
-                },
-                stateSnapshot: x.stateSnapshot
-            })
+            mergeMap(({action}) => 
+                this.apiService.put<void>(`${ApiUrl.Users}/${action.userName}/NewPassword`, action)),
         )
     }
 }
