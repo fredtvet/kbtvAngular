695d5964e7d1d64ad6cc931b832a7d6a868f0419
Fredrik Tvete <fretve@hotmail.com>
2021-04-25 20:14:02 +0200

    use indexof instead of set

--- apps/kbtv-client/src/app/core/http-retry.strategy.ts
+++ apps/kbtv-client/src/app/core/http-retry.strategy.ts
@@ -8,12 +8,11 @@ export const httpRetryStrategy = ({
     excludedStatusCodes = []
   }: {maxRetryAttempts?: number, scalingDuration?: number, excludedStatusCodes?: number[]} = {}) => 
   (attempts: Observable<HttpErrorResponse>) => {
-    const excludedStatusCodeMap = new Set(excludedStatusCodes);
     return attempts.pipe(
       mergeMap((error, i) => {
         const retryAttempt = i + 1;
 
-        if(excludedStatusCodeMap.has(error.status) || retryAttempt > maxRetryAttempts) 
+        if((excludedStatusCodes.indexOf(error.status) !== -1) || retryAttempt > maxRetryAttempts) 
             return throwError(error);
         
         return timer(retryAttempt * scalingDuration);
