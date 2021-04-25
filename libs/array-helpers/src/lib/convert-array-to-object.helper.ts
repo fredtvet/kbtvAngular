6239f268c6b8ade2bd7c5fda35b7382a6ea48761
Fredrik Tvete <fretve@hotmail.com>
2021-04-25 20:12:25 +0200

    add no key option if no obj & memoize

--- libs/array-helpers/src/lib/convert-array-to-object.helper.ts
+++ libs/array-helpers/src/lib/convert-array-to-object.helper.ts
@@ -1,35 +1,40 @@
 import { Immutable, ImmutableArray, Maybe, Prop} from 'global-types';
-
+import { _weakMemoizer } from './weak-memoizer.helper';
 
 type Response<T> = { [key: string]: Immutable<T> }
+
+export const _convertArrayToObject = _weakMemoizer(convertArrayToObject)
+
 /**
  * Convert an array to a key/value object
  * @param array 
  * @param key An item property or function used as key in the object. If null, the value is used as key.
  * @returns An object containing keys with accociated values
  */
-export function _convertArrayToObject<T>(
+function convertArrayToObject<T>(
   array: Maybe<ImmutableArray<T>>, 
-  key: Prop<Immutable<T>> | ((t: Immutable<T>) => string)  
+  key?: Prop<Immutable<T>> | ((t: Immutable<T>) => string) | null
 ): Response<T> {
     if(!array?.length) return {};
-    
+
     const result: Response<T> = {}; 
     
     let setter: (entity: Immutable<T>, result: Response<T>) => void;
 
     switch(typeof key){
       case "string": 
-        setter = (entity: Immutable<T>, result: Response<T>) => result[<string> entity[key]] = entity; 
+        setter = (entity, result) => result[<string> entity[key]] = entity; 
         break;
       case "function":
-        setter = (entity: Immutable<T>, result: Response<T>) => result[key(entity)] = entity
+        setter = (entity, result) => result[(<(t: Immutable<T>) => string> key)(entity)] = entity
         break;
       default:
-        throw new Error("Key must be of type string or function")
+        setter = (entity, result) => result[entity as unknown as string] = entity
+        break;
     }
 
     for(var i = 0; i < array.length; i++) setter(array[i], result);
 
     return result;
 }
+
