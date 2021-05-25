79db2ac0c228d9b4e2ed9d5c5be21dece53dccce
Fredrik Tvete <fretve@hotmail.com>
2021-05-25 16:52:38 +0200

    use api service instead of httpclient mailmodels

--- apps/kbtv-client/src/app/core/state/mail-models/mail-models.http.effect.ts
+++ apps/kbtv-client/src/app/core/state/mail-models/mail-models.http.effect.ts
@@ -1,10 +1,9 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ModelBaseUrls } from '@core/configurations/model/model-base-urls.const';
 import { Model } from '@core/models';
+import { ApiService } from '@core/services/api.service';
 import { Observable } from 'rxjs';
 import { mergeMap } from 'rxjs/operators';
-import { environment } from 'src/environments/environment';
 import { DispatchedAction, Effect, listenTo } from 'state-management';
 import { MailModelsHttpRequest } from './mail-models-http-request.interface';
 import { MailModelsAction } from './mail-models.action';
@@ -12,14 +11,14 @@ import { MailModelsAction } from './mail-models.action';
 @Injectable()
 export class MailModelsHttpEffect implements Effect<MailModelsAction<Model>>{
 
-    constructor(private httpClient: HttpClient) {}
+    constructor(private apiService: ApiService) {}
     
     handle$(actions$: Observable<DispatchedAction<MailModelsAction<Model>>>): Observable<void> {
         return actions$.pipe(
             listenTo([MailModelsAction]),
             mergeMap(({action}) => 
-                this.httpClient.post<void>(
-                    environment.apiUrl + ModelBaseUrls[action.stateProp] + "/Mail", 
+                this.apiService.post<void>(
+                    ModelBaseUrls[action.stateProp] + "/Mail", 
                     <MailModelsHttpRequest> {ids: action.ids, toEmail: action.toEmail}
                 )),
         )
