1009400895f9b839fd4b50997b8c735ddd33b8b2
Fredrik Tvete <fretve@hotmail.com>
2021-05-27 23:28:11 +0200

    ignore blob urls in file extension pipe

--- apps/kbtv-client/src/app/feature-modules/mission-modules/mission-document-list/pipes/file-extension.pipe.ts
+++ apps/kbtv-client/src/app/feature-modules/mission-modules/mission-document-list/pipes/file-extension.pipe.ts
@@ -1,13 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Maybe } from 'global-types';
 
-@Pipe({
-  name: 'fileExtension'
-})
+@Pipe({name: 'fileExtension'})
 export class FileExtensionPipe implements PipeTransform {
 
   transform(value: Maybe<string>): string {
-    return value?.split('.').pop() || "";
+    if(!value) return "";
+    if(value.substr(0, 4) === "blob") return "";
+    return value.split('.').pop() || "";
   }
 
 }
