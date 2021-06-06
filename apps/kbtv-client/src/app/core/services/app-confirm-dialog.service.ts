9ac1d1e86fa3071146b68779227379ddcdc29d69
Fredrik Tvete <fretve@hotmail.com>
2021-06-06 13:29:46 +0200

    fix lazy import observables to prevent loading instantly

--- apps/kbtv-client/src/app/core/services/app-confirm-dialog.service.ts
+++ apps/kbtv-client/src/app/core/services/app-confirm-dialog.service.ts
@@ -1,18 +1,22 @@
 import { Compiler, Injectable, Injector } from '@angular/core';
 import { _tryWithLogging } from 'array-helpers';
 import { ConfirmDialogService } from 'confirm-dialog';
-import { from, Observable } from 'rxjs';
-import { shareReplay } from 'rxjs/operators';
+import { from, Observable, of } from 'rxjs';
+import { map, shareReplay, switchMap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'any' })
 export class AppConfirmDialogService {
   
   dialog$: Observable<ConfirmDialogService> =
-    from(import('confirm-dialog').then(({ConfirmDialogModule, ConfirmDialogService}) => {
-      const moduleFactory = this.compiler.compileModuleSync(ConfirmDialogModule);   
-      const moduleInstance = moduleFactory.create(this.injector);
-      return moduleInstance.injector.get(ConfirmDialogService);
-    })).pipe(shareReplay())
+    of(null).pipe(
+      switchMap(x => from(import('confirm-dialog'))),
+      map(({ConfirmDialogModule, ConfirmDialogService}) => {
+        const moduleFactory = this.compiler.compileModuleSync(ConfirmDialogModule);   
+        const moduleInstance = moduleFactory.create(this.injector);
+        return moduleInstance.injector.get(ConfirmDialogService);
+      }),
+      shareReplay()
+    )
 
   constructor(
     private compiler: Compiler,
--- apps/kbtv-client/src/app/core/services/app-dialog.service.ts
+++ apps/kbtv-client/src/app/core/services/app-dialog.service.ts
@@ -1,17 +1,21 @@
 import { Compiler, Injectable, Injector } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { from, Observable } from 'rxjs';
-import { shareReplay } from 'rxjs/operators';
+import { from, Observable, of } from 'rxjs';
+import { map, shareReplay, switchMap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'any' })
 export class AppDialogService {
   
     dialog$: Observable<MatDialog> =
-        from(import('@angular/material/dialog').then(({MatDialogModule, MatDialog}) => {
-            const moduleFactory = this.compiler.compileModuleSync(MatDialogModule);  
+        of(null).pipe(
+            switchMap(x => from(import('@angular/material/dialog'))),
+            map(({MatDialogModule, MatDialog}) => {
+            const moduleFactory = this.compiler.compileModuleSync(MatDialogModule);   
             const moduleInstance = moduleFactory.create(this.injector);
-            return moduleInstance.injector.get(MatDialog) 
-        })).pipe(shareReplay())
+            return moduleInstance.injector.get(MatDialog);
+            }),
+            shareReplay()
+        )
 
     constructor(
         private compiler: Compiler,
